788f6426ec281eff9dc73109a77349412bf5ce4f
viincyy <viincyy126@gmail.com>
2021-08-27 00:22:53 +0700

    generics

--- typescript/index.js
+++ typescript/index.js
@@ -1,8 +1,89 @@
 // GENERICS
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var last = function (arr) { return arr[arr.length - 1]; };
 // const l1 = last[1]
 // console.log(l1)
 var lastT = function (arr) { return arr[arr.length - 1]; };
 var l3 = lastT([1, 2, 3]);
 var l4 = lastT(['a', 'b']);
-console.log(l4);
+// console.log(l4)
+var makeArr = function (x) { return [x]; };
+var m = makeArr(5);
+// console.log(m)
+var makeArrT = function (x) { return [x]; };
+var m2 = makeArrT('b');
+console.log(m2);
+var makeArrXY = function (x, y) { return [x, y]; };
+var m4 = makeArrXY(5, 'a');
+console.log(m4);
+var makeTuple = function (x, y) { return [x, y]; };
+var m5 = makeTuple(5, 'a');
+var m6 = makeTuple('a', 123);
+var m7 = makeTuple('a', false);
+var makeTupleWithDefault = function (x, y) { return [x, y]; };
+var m8 = makeTupleWithDefault('a', 3);
+// Generics extends
+var makeFullNameConstraint = function (obj) { return (__assign(__assign({}, obj), { fullName: obj.firstName + " " + obj.lastName })); };
+var n1 = makeFullNameConstraint({
+    firstName: 'S',
+    lastName: 'N'
+});
+var makeFullNameConstraintWithGenerics = function (obj) { return (__assign(__assign({}, obj), { fullName: obj.firstName + " " + obj.lastName })); };
+var n3 = makeFullNameConstraintWithGenerics({ firstName: 'S', lastName: 'N', age: 30, gender: 'male' });
+var addNewEmployee = function (employee) {
+    var uid = Math.floor(Math.random() * 100);
+    return __assign(__assign({}, employee), { uid: uid });
+};
+var employee_01 = addNewEmployee({
+    name: 'S',
+    age: 30
+});
+console.log(employee_01);
+// generics extends
+var addNewEmployeeT = function (employee) {
+    var uid = Math.floor(Math.random() * 100);
+    return __assign(__assign({}, employee), { uid: uid });
+};
+var employee_02 = addNewEmployeeT({
+    name: 'Suu',
+    age: 30
+});
+console.log(employee_02.name);
+var addNewEmployeeTWithConstraint = function (employee) {
+    var uid = Math.floor(Math.random() * 100);
+    return __assign(__assign({}, employee), { uid: uid });
+};
+var employee_03 = addNewEmployeeTWithConstraint({
+    name: 'Suu',
+    age: 30
+});
+var resourceOne = {
+    uid: 1,
+    name: 'SN',
+    data: 'Ahhh'
+};
+var resourceTwo = {
+    uid: 1,
+    name: 'SN',
+    data: {}
+};
+var number2 = {
+    uid: 2,
+    name: '2',
+    data: [1, 2, 3]
+};
+var resourceThree = {
+    uid: 1,
+    name: 'SN',
+    data: ['a', 'b', 'c']
+};
--- typescript/index.ts
+++ typescript/index.ts
@@ -11,4 +11,124 @@ const last = (arr: Array<number>) => arr[arr.length - 1]
 const lastT = <T>(arr: Array<T>) => arr[arr.length - 1]
 const l3 = lastT([1, 2, 3]);
 const l4 = lastT(['a', 'b']);
-console.log(l4)
+// console.log(l4)
+
+const makeArr = (x:number) => [x]
+const m = makeArr(5);
+// console.log(m)
+
+const makeArrT = <T>(x: T) => [x]
+const m2 = makeArrT('b');
+console.log(m2)
+
+const makeArrXY = <X, Y>(x: X, y: Y) => [x, y]
+const m4 = makeArrXY(5, 'a');
+console.log(m4)
+
+const makeTuple = <X, Y>(x: X, y: Y): [X, Y] => [x, y]
+const m5 = makeTuple(5, 'a');
+const m6 = makeTuple<string, number>('a', 123);
+const m7 = makeTuple<string | null, number | boolean>('a', false);
+
+const makeTupleWithDefault = <X, Y = number>(x: X, y: Y): [X, Y] => [x, y]
+const m8 = makeTupleWithDefault<string | null>('a', 3); 
+
+
+// Generics extends
+
+const makeFullNameConstraint = (obj: {
+    firstName: string,
+    lastName: string
+}) => ({
+    ...obj,
+    fullName: `${obj.firstName} ${obj.lastName}`
+})
+
+const n1 = makeFullNameConstraint({
+    firstName: 'S',
+    lastName: 'N'
+})
+
+const makeFullNameConstraintWithGenerics = <T extends {firstName: string, lastName: string}>(obj: T) => ({
+    ...obj,
+    fullName: `${obj.firstName} ${obj.lastName}`
+})
+
+const n3 = makeFullNameConstraintWithGenerics({firstName: 'S', lastName: 'N', age: 30, gender: 'male'})
+
+const addNewEmployee = (employee: object) => {
+    const uid = Math.floor(Math.random() * 100)
+    return {
+        ...employee,
+        uid
+    }
+}
+
+const employee_01 = addNewEmployee({
+    name: 'S',
+    age: 30
+})
+
+console.log(employee_01);
+
+// generics extends
+const addNewEmployeeT = <T extends object>(employee: T) => {
+    const uid = Math.floor(Math.random() * 100)
+    return {
+        ...employee,
+        uid
+    }
+}
+
+const employee_02 = addNewEmployeeT({
+    name: 'Suu',
+    age: 30
+})
+
+console.log(employee_02.name);
+
+const addNewEmployeeTWithConstraint = <T extends {name: string}>(employee: T) => {
+    const uid = Math.floor(Math.random() * 100)
+    return {
+        ...employee,
+        uid
+    }
+}
+
+const employee_03 = addNewEmployeeTWithConstraint({
+    name: 'Suu',
+    age: 30
+})
+
+// GENERICS INTERFACE
+interface Resource<T> {
+    uid: number,
+    name: string,
+    data:  T
+}
+
+const resourceOne: Resource<string> = {
+    uid: 1,
+    name: 'SN',
+    data: 'Ahhh'
+}
+
+const resourceTwo: Resource<object> = {
+    uid: 1,
+    name: 'SN',
+    data: {}
+}
+
+type NumberResource = Resource<number[]>
+
+const number2: NumberResource = {
+    uid: 2,
+    name: '2',
+    data: [1, 2, 3]
+}
+
+const resourceThree: Resource<string[]> = {
+    uid: 1,
+    name: 'SN',
+    data: ['a', 'b', 'c']
+}
