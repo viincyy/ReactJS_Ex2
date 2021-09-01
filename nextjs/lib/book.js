7fdec07edff1d9d0b616922a1c2ba4d298f5770a
viincyy <viincyy126@gmail.com>
2021-09-01 23:42:09 +0700

    Read content from book

---
+++ nextjs/lib/book.js
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+
+// Get all book files from books folder
+const booksDir = path.join(process.cwd(), 'books');
+
+export const getBook = () => {
+    // Read file
+    const bookFileNames = fs.readdirSync(booksDir);
+    const bookData = bookFileNames.map((bookFileName) => {
+        const fileBookPath = path.join(booksDir, bookFileName);
+        const bookContent = fs.readFileSync(fileBookPath, 'utf-8');
+
+        return {
+            bookName: bookFileName.replace(/\.txt$/, ''),
+            bookContent,
+        };
+    });
+    return bookData;
+};
