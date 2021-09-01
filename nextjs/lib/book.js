9fff821ab8a6023a388b76eb0617ce7d6b705751
viincyy <viincyy126@gmail.com>
2021-09-01 23:46:41 +0700

    Book route

--- nextjs/lib/book.js
+++ nextjs/lib/book.js
@@ -1,20 +1,36 @@
-import fs from 'fs';
-import path from 'path';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Layout from '../../components/Layout';
+import Link from 'next/link';
+import { getBooks } from '../../lib/book';
 
-// Get all book files from books folder
-const booksDir = path.join(process.cwd(), 'books');
-
-export const getBook = () => {
-    // Read file
-    const bookFileNames = fs.readdirSync(booksDir);
-    const bookData = bookFileNames.map((bookFileName) => {
-        const fileBookPath = path.join(booksDir, bookFileName);
-        const bookContent = fs.readFileSync(fileBookPath, 'utf-8');
-
-        return {
-            bookName: bookFileName.replace(/\.txt$/, ''),
-            bookContent,
-        };
-    });
-    return bookData;
+const Books = ({ books }) => {
+    return (
+        <Layout>
+            {books.map((book) => (
+                <Card className='my-3 shadow' key={book.bookName}>
+                    <Card.Body>
+                        <Card.Title>{book.bookName}</Card.Title>
+                        <Card.Text>{book.bookContent}</Card.Text>
+                        <Link href='/'>
+                            <Button variant='dark'>Back</Button>
+                        </Link>
+                    </Card.Body>
+                </Card>
+            ))}
+        </Layout>
+    );
 };
+
+// Du lieu phu thuoc vao moi request, nhung ma van tao ra HTML tinh cho front-end, nen van tot cho SEO
+export const getStaticProps = async () => {
+    const books = await getBooks();
+    console.log(books);
+    return {
+        props: {
+            books,
+        },
+    };
+};
+
+export default Books;
